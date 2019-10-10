61b097c90c42a06bec225fbc5e58784daee11f78
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2019-10-10 19:31:01 +0530

    Base.html error

--- Django_blog/Django_blog/wsgi.py
+++ Django_blog/Django_blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_project project.
+WSGI config for Django_blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_blog.settings')
 
 application = get_wsgi_application()
