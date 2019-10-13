436f0114bc063c8a24ca5cd7bc332d94043339e7
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2019-10-13 12:45:04 +0530

    Fixed

--- Django_blog/blog/views.py
+++ Django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.views import generic
 from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'blog/index.html'
 
 class PostDetail(generic.DetailView):
     model = Post
